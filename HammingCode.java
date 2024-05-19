8f571a0d83e71502ef07bbec6886082d3f72a6a0
gecgelenus <44176143+gecgelenus@users.noreply.github.com>
2024-05-19 15:03:22 +0300

    Son surum

---
+++ HammingCode.java
@@ -0,0 +1,19 @@
+
+package hammingcode;
+
+
+
+public class HammingCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true);
+        mainframe.setResizable(false);
+        mainframe.initArray();
+
+    }
+    
+}
